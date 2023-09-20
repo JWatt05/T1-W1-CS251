fc39c30dd81aef7263161961763be8c658d5db9b
wlb22171 <j.watt.2022@uni.strath.ac.uk>
2023-09-20 22:53:55 +0100

    inital push

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
+    public void helloWorld(){
 
-
-
-
+        int z = 0;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
